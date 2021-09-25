9e8d0d2483a376a1ca2e24438d9ad87ca866344a
Wil <wilbertirwan@gmail.com>
2021-09-25 15:09:52 +0700

    day1

---
+++ helloworld.py
@@ -0,0 +1,21 @@
+#test
+print("new 12 world")
+# print("new 12 world")
+# print("new 12 world")
+# print("new 12 world")
+# print("new 12 world")
+
+x = 5
+y = "hello world"
+z = 19.3
+print(x)
+print(y)
+
+print(type(x))
+print(type(z))
+
+asd = float(x)
+print(type(asd))
+
+n = 2*4/3
+print(int(n))
